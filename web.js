27c7701e742b5dc7651958f92b31c20e8c68e350
davefedele <dave.fedele@gmail.com>
2013-07-13 22:38:40 -0700

    Read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
 
+var fs = require('fs');
+
+var infile = "index.html";
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+    var text = fs.readFileSunc(infile);
+    response.send(text);
 });
 
 var port = process.env.PORT || 5000;
